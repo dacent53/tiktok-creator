e736d205f4b66938efc59c73a0e669281bb258de
alethino <gneo0194@gmail.com>
2025-09-20 13:41:56 +0300

    Update TikTok.py

--- TikTok.py
+++ TikTok.py
@@ -1,7 +1,6 @@
 # TikTok-gen-accounts
 
 # tiktokapi
-
 tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device register, tiktok api
 # TikTok Private API
 # Tiktok (Source Code)
